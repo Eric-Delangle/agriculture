35cee0e4174613d93dfc70a6059ad32ff76b6fef
Eric-Delangle <polvu@hotmail.fr>
2020-09-08 13:41:08 +0200

    test

---
+++ src/Controller/ShopBundle/HomePageController.php
@@ -0,0 +1,37 @@
+<?php
+
+/*
+ * This file is part of the Sylius package.
+ *
+ * (c) Paweł Jędrzejewski
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Templating\EngineInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+final class HomepageController
+{
+    /** @var EngineInterface */
+    private $templatingEngine;
+
+    public function __construct(EngineInterface $templatingEngine)
+    {
+        $this->templatingEngine = $templatingEngine;
+    }
+
+    public function indexAction(Request $request): Response
+    {
+        if (isset($_POST['mot_de_passe']) AND $_POST['mot_de_passe'] ==  "password") // Si le mot de passe est bon
+        {
+        return $this->templatingEngine->renderResponse('@SyliusShop/Homepage/index.html.twig');
+        }
+    }
+}
