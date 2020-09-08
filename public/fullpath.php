c4843c8022452f9457452565b85c97389a71f377
Eric-Delangle <polvu@hotmail.fr>
2020-09-08 19:11:16 +0200

    test

---
+++ public/fullpath.php
@@ -0,0 +1,5 @@
+<? php
+$ dir = nom_rép (__ FILE__);
+echo "<p> Chemin complet vers ce répertoire:". $ dir. "</p>";
+echo "<p> Chemin complet vers un fichier .htpasswd dans ce répertoire:". $ dir. "/.htpasswd". "</p>";
+?>
